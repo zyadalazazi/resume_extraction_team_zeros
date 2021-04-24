2591f248a6e00b2107569366d8d8512e599116ef
Swapnil Vishwakarma <swapnil.vishwakarma18@siesgst.ac.in>
2021-04-24 15:41:02 +0530

    Solved YAML File problem

--- testutility.py
+++ testutility.py
@@ -1,10 +1,18 @@
+import logging
+import os
+import subprocess
+import yaml
+import pandas as pd
+import datetime 
+import gc
+import re
 
 def read_yml_file(file_path):
     with open(file_path, 'r') as stream:
         try:
-            return yaml.load(stream)
-        except yaml.YAMLError as exec:
-            logging.error(exec)
+            return yaml.safe_load(stream)
+        except yaml.YAMLError as exc:
+            logging.error(exc)
 
 def replacer(string, char):
     pattern = char + '{2,}'
@@ -12,22 +20,42 @@ def replacer(string, char):
     return string
 
 def col_validate(df, col_config):
-    cols = df.columns
-    cols = cols.str.replace(' ', '_')
-    cols = list(map(lambda x: replacer(x, '_'), list(cols)))
-    expected_cols = list(map(lambda x: x.lower(), col_config['columns']))
-    cols = list(map(lambda x: x.lower(), list(cols)))
-    
-    if len(cols) == len(expected_cols) and list(cols) == list(expected_cols):
-        print("\nColumn name and column length are successfully validated!")
-        return 1
+  cols = df.columns
+  cols = cols.str.strip()
+  cols.str.replace("_", "")
+  cols = list(map(lambda x: replacer(x, ' '), list(cols)))
+  expected_col = list(map(lambda x: x.lower(),  col_config['columns']))
+  cols.sort()
+  expected_col.sort()
+  if len(cols) == len(expected_col) and cols == expected_col:
+    print("Column name and Column length Validation Passed!!")
+    return 1
+  else:
+    print("Column name and Column length Validation Failed..")
+    mismatched_columns_file = list(set(cols).difference(expected_col))
+    print("Following File columns are not in the YAML file", mismatched_columns_file)
+    missing_YAML_file = list(set(expected_col).difference(cols))
+    print("Following YAML columns are not in the file uploaded", missing_YAML_file)
+    logging.info(f'df columns: {cols}')
+    logging.info(f'expected columns: {expected_col}')
+    return 0
 
-    else:
-        print("Column name and column length have not passed the validation test")
-        mismatched_columns = list(set(colms).difference(expected_col))
-        print("The following columns are missing from the YAML file", mismatched_columns)
-        missing_file = list(set(expected_col).difference(colms))
-        print("The followning columns are missing from the file uploaded", missing_file)
-        logging.info(f'df columns: {colms}')
-        logging.info(f'expected columns: {expected_col}')
-        return 0
+# Function to get the size of dataset
+def humanbytes(B):
+  'Return the given bytes as a human friendly KB, MB, GB, or TB string'
+  B = float(B)
+  KB = float(1024)
+  MB = float(KB ** 2) # 1,048,576
+  GB = float(KB ** 3) # 1,073,741,824
+  TB = float(KB ** 4) # 1,099,511,627,776
+
+  if B < KB:
+    return '{0} {1}'.format(B,'Bytes' if 0 == B > 1 else 'Byte')
+  elif KB <= B < MB:
+    return '{0:.2f} KB'.format(B/KB)
+  elif MB <= B < GB:
+    return '{0:.2f} MB'.format(B/MB)
+  elif GB <= B < TB:
+    return '{0:.2f} GB'.format(B/GB)
+  elif TB <= B:
+    return '{0:.2f} TB'.format(B/TB)
