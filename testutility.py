980d14de7634fa8f75582d6636a35c7c79e89720
zyadalazazi <zyadazazi@gmail.com>
2021-05-21 11:02:19 +0300

    Adding EDA Document

--- testutility.py
+++ testutility.py
@@ -20,42 +20,42 @@ def replacer(string, char):
     return string
 
 def col_validate(df, col_config):
-  cols = df.columns
-  cols = cols.str.strip()
-  cols.str.replace("_", "")
-  cols = list(map(lambda x: replacer(x, ' '), list(cols)))
-  expected_col = list(map(lambda x: x.lower(),  col_config['columns']))
-  cols.sort()
-  expected_col.sort()
-  if len(cols) == len(expected_col) and cols == expected_col:
-    print("Column name and Column length Validation Passed!!")
-    return 1
-  else:
-    print("Column name and Column length Validation Failed..")
-    mismatched_columns_file = list(set(cols).difference(expected_col))
-    print("Following File columns are not in the YAML file", mismatched_columns_file)
-    missing_YAML_file = list(set(expected_col).difference(cols))
-    print("Following YAML columns are not in the file uploaded", missing_YAML_file)
-    logging.info(f'df columns: {cols}')
-    logging.info(f'expected columns: {expected_col}')
-    return 0
+    cols = df.columns
+    cols = cols.str.strip()
+    cols.str.replace("_", "")
+    cols = list(map(lambda x: replacer(x, ' '), list(cols)))
+    expected_col = list(map(lambda x: x.lower(),  col_config['columns']))
+    cols.sort()
+    expected_col.sort()
+    if len(cols) == len(expected_col) and cols == expected_col:
+        print("Column name and Column length Validation Passed!!")
+        return 1
+    else:
+        print("Column name and Column length Validation Failed..")
+        mismatched_columns_file = list(set(cols).difference(expected_col))
+        print("Following File columns are not in the YAML file", mismatched_columns_file)
+        missing_YAML_file = list(set(expected_col).difference(cols))
+        print("Following YAML columns are not in the file uploaded", missing_YAML_file)
+        logging.info(f'df columns: {cols}')
+        logging.info(f'expected columns: {expected_col}')
+        return 0
 
 # Function to get the size of dataset
 def humanbytes(B):
-  'Return the given bytes as a human friendly KB, MB, GB, or TB string'
-  B = float(B)
-  KB = float(1024)
-  MB = float(KB ** 2) # 1,048,576
-  GB = float(KB ** 3) # 1,073,741,824
-  TB = float(KB ** 4) # 1,099,511,627,776
+    'Return the given bytes as a human friendly KB, MB, GB, or TB string'
+    B = float(B)
+    KB = float(1024)
+    MB = float(KB ** 2) # 1,048,576
+    GB = float(KB ** 3) # 1,073,741,824
+    TB = float(KB ** 4) # 1,099,511,627,776
 
-  if B < KB:
-    return '{0} {1}'.format(B,'Bytes' if 0 == B > 1 else 'Byte')
-  elif KB <= B < MB:
-    return '{0:.2f} KB'.format(B/KB)
-  elif MB <= B < GB:
-    return '{0:.2f} MB'.format(B/MB)
-  elif GB <= B < TB:
-    return '{0:.2f} GB'.format(B/GB)
-  elif TB <= B:
-    return '{0:.2f} TB'.format(B/TB)
+    if B < KB:
+        return '{0} {1}'.format(B,'Bytes' if 0 == B > 1 else 'Byte')
+    elif KB <= B < MB:
+        return '{0:.2f} KB'.format(B/KB)
+    elif MB <= B < GB:
+        return '{0:.2f} MB'.format(B/MB)
+    elif GB <= B < TB:
+        return '{0:.2f} GB'.format(B/GB)
+    elif TB <= B:
+        return '{0:.2f} TB'.format(B/TB)
